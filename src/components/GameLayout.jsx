303853e33a6fb9622379dd85160fdf8fc91b1f29
Sardor Botirov <sbotiroff@Sardors-MBP.hsd1.wa.comcast.net>
2019-09-04 21:53:50 -0700

    added style and modified folder structure

--- src/components/GameLayout.jsx
+++ src/components/GameLayout.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import PlayerTurn from './PlayerTurn';
-import WinnerStatus from './WinnerStatus';
 import TicTacToeContainer from './TicTacToeContainer';
 
 function GameLayout(){
     return(
         <div>
             <TicTacToeContainer />
-            <PlayerTurn />
-            <WinnerStatus />
         </div>
     );
 }
--- src/components/TicTacToeHeader.jsx
+++ src/components/TicTacToeHeader.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './TicTacToeHeader.css';
 
 function TicTacToeHeader() {
     return (
         <header>
             <h1>Tic Tac Toe Game</h1>
+            <hr/>
         </header>
     );
 
