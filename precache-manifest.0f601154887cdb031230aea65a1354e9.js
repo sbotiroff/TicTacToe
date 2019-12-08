6c3b1e36ca5e472d275c7fba7d062613c445d614
Sardor Botirov <sbotiroff@Sardors-MBP.hsd1.wa.comcast.net>
2019-12-07 16:54:38 -0800

    Updates

---
+++ precache-manifest.0f601154887cdb031230aea65a1354e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4318ff9a37d2a69d62f2f850869bfd",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "4fe71ef1abe65b3122eb",
+    "url": "/TicTacToe/static/css/main.d7a464a3.chunk.css"
+  },
+  {
+    "revision": "a8d0230a3774372e3d65",
+    "url": "/TicTacToe/static/js/2.6fcad12b.chunk.js"
+  },
+  {
+    "revision": "4fe71ef1abe65b3122eb",
+    "url": "/TicTacToe/static/js/main.43e8c262.chunk.js"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  }
+]);
